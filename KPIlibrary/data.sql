8c35d56bc2c98c74ee52657e80786004d2e77795
NastyaStrashnova <60945623+NastyaStrashnova@users.noreply.github.com>
2020-05-31 00:08:04 +0300

    Create data.sql

---
+++ KPIlibrary/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO kpilibrary.code (`code_id`, name, `action`) VALUES ('MN45673789', 'Ivanov Ivan', '0');
+INSERT INTO kpilibrary.code (`code_id`, name, `action`) VALUES ('JU78978908', 'Kovalenko Oleksandra', '0');
+INSERT INTO kpilibrary.code (`code_id`, name, `action`) VALUES ('HD73848598', 'Kovalchuk Olesia', '0');
+INSERT INTO kpilibrary.code (`code_id`, name, `action`) VALUES ('TA76545678', 'Bobovich Bob', '0');
+
+
+INSERT INTO kpilibrary.books (`title`, author, `year`) VALUES ('title_1', 'author_1', '1999');
+INSERT INTO kpilibrary.books (`title`, author, year, `count`) VALUES ('title_2', 'author_1', '2008', '15');
+INSERT INTO kpilibrary.books (`title`, author, year, `count`) VALUES ('title_1', 'author_2', '1898', '14');
